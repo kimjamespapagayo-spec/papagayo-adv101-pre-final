a325852a17f4bafce00e3e9c6e355a2090d5f860
kimjames.papagayo@hcdc.edu.ph <hertzianjay.cuervo@hcdc.edu.ph>
2025-11-22 15:26:01 +0800

    first

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'
+import Head from 'next/head'
+import { Geist, Geist_Mono } from 'next/font/google'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const geistSans = Geist({
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
+})
+
+export default function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<div className={`${geistSans.variable} ${geistMono.variable} min-h-screen`}> 
+				<Component {...pageProps} />
+			</div>
+		</>
+	)
 }
+
